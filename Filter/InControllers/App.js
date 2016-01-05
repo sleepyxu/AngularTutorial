6e059673d1de13c4197aea9e01e6f5723ea320fe
Roger Xu <sleepyxu@hotmail.com>
2016-01-05 13:36:44 +0800

    Add filter

---
+++ Filter/InControllers/App.js
@@ -0,0 +1,12 @@
+﻿angular.module('FilterInControllerModule', []).
+controller('FilterController', ['filterFilter', function (filterFilter) {
+    this.array = [
+      { name: 'Tobias' },
+      { name: 'Jeff' },
+      { name: 'Brian' },
+      { name: 'Igor' },
+      { name: 'James' },
+      { name: 'Brad' }
+    ];
+    this.filteredArray = filterFilter(this.array, 'a');
+}]);
