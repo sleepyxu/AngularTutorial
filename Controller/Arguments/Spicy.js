837f5bf8e1ea335284f6121e4487811c60d3ae11
Roger Xu <sleepyxu@hotmail.com>
2016-01-05 12:55:34 +0800

    Add controller arguments

---
+++ Controller/Arguments/Spicy.js
@@ -0,0 +1,10 @@
+﻿var myApp = angular.module('spicyApp', []);
+
+myApp.controller('SpicyController', ['$scope', function ($scope) {
+    $scope.customSpice = "wasabi";
+    $scope.spice = 'very';
+
+    $scope.spicy = function (spice) {
+        $scope.spice = spice;
+    };
+}]);
